c401693284a6e3059a67d2c08477a24f96731e0b
Grifin Butler <gbutler0941@floridapoly.edu>
2022-11-11 17:07:46 -0500

    Expanded Sanctuary 1

---
+++ .GameAreas/StartingArea/Unknown/Platform/Next_Platform/~Advance.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#dialogue
+echo "The witch collapses on the ground and the world fades out again."
+echo "--------------------------------"
+echo "She is awoken once more by a stern voice"
+
+cd ../../../../Sanctuaries/Sanctuary1
+
+#change areas to the first Sanctuary Area
--- Start.sh
+++ Start.sh
@@ -8,13 +8,15 @@ echo "Witch sees a dragonfly that she needs"
 sleep 1
 echo "Witch grabs dragonfly and it panics, sending them both into the Dark World"
 sleep 1
+
 echo "--------------------------------------"
+echo "The following commands are the spells in your repetoire:"
 echo "Use ls to look around."
 echo "use cat to view files with a .txt extension."
 echo "--------------------------------------"
 
 #use . ./Start.sh
-cd .GameAreas/Area1
+cd .GameAreas/StartingArea/Unknown
 
 #for timer
 # exec a second script in the background ./script&
