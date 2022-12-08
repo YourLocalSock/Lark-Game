fcf095628068e20afbfe7a2f2d80f7ba16133fc2
Grifin Butler <gbutler0941@floridapoly.edu>
2022-12-07 19:52:27 -0500

    12 - 7 - 22 Update

--- .test.sh
+++ .test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 count=0
-while [ $count -le 5 ]; do
+until [ $count -gt 5 ]; do
 	read input
 	if [[ $input != "endScript.sh" ]]; then
 		let count++
 	else
 		echo "Complete"
 		break
- fi
+	fi
 done
 
 if [ $count -gt 5 ]; then
